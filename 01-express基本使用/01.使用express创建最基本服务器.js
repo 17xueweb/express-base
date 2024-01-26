2b7d92d12dd23e2a28019dae6294e7d308f82f7d
lcruan <1416802331@qq.com>
2024-01-26 21:25:26 +0800

    通过req.params 获取url中/1这种形式的动态参数

--- 01-express基本使用/01.使用express创建最基本服务器.js
+++ 01-express基本使用/01.使用express创建最基本服务器.js
@@ -18,11 +18,21 @@ app.post('/user', (req, res) => {
     res.send('请求成功')
 })
 
+// req.query 获取 url中 携带的参数
 app.get('/', (req, res) => {
     // 通过 req.query 可以获取客户端发送过来的 查询参数
     // 注意：默认情况下, req.query 是一个空对象
     console.log(req.query);
     res.send(req.query)
+})
+
+// req.params 获取 url 中的动态参数 比如 url为http://127.0.0.1:80/1 -> 获取结果为 { id: 1}
+// 接口中通过 ”:变量“ 写法
+// 注意写法 '/user/:id' :id前面要有个斜杠
+app.get('/user/:id', (req, res) => {
+    // req.params 是动态匹配到的 URL 参数，默认也是一个空对象
+    console.log(req.params);
+    res.send(req.params)
 })
 
 // 3. 启动 web 服务器 
