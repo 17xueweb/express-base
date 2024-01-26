521bae31fb6959a4781177f75c5d53877a900274
lcruan <1416802331@qq.com>
2024-01-26 23:41:21 +0800

    全局中间件的简化写法

--- 03-express中间件/01.定义最简单的中间件函数.js
+++ 03-express中间件/01.定义最简单的中间件函数.js
@@ -3,13 +3,19 @@ const express = require('express')
 const app = express()
 
 // 定义一个最简单的中间件函数
-const mw = function(req, res, next) {
+// const mw = function(req, res, next) {
+//     console.log('这是最简单的中间件函数');
+//     next()
+// }
+
+// // 将 mw 注册为全局生效的中间件
+// app.use(mw)
+
+// 全局中间件的简化写法
+app.use((req, res, next) => {
     console.log('这是最简单的中间件函数');
     next()
-}
-
-// 将 mw 注册为全局生效的中间件
-app.use(mw)
+})
 
 app.get('/', (req, res) => {
     console.log('调用了 / 这个路由');
