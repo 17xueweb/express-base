b92577743f7ab36e0a610a7ed967759f319a95d4
lcruan <1416802331@qq.com>
2024-01-26 21:00:07 +0800

    express 监听客户端的get和post请求

--- 01-express基本使用/01.使用express创建最基本服务器.js
+++ 01-express基本使用/01.使用express创建最基本服务器.js
@@ -2,7 +2,23 @@
 const express = require('express')
 // 2. 创建 web 服务器
 const app = express()
-// 3. 启动 web 服务器
+
+// 4. 监听客户端的 GET 和 POST 请求，并向客户端响应具体内容
+app.get('/user', (req, res) => {
+    // 调用 express() 提供的 res.send() 方法，向客户端响应一个 JSON 对象
+    res.send({
+        name: 'zs',
+        age: 18,
+        gender: '男'
+    })
+})
+
+app.post('/user', (req, res) => {
+    // 调用 express() 提供的 res.send() 方法，向客户端响应一个 文本字符串
+    res.send('请求成功')
+})
+
+// 3. 启动 web 服务器 
 app.listen(80, () => {
     console.log('server running at http://127.0.0.1');
 })
