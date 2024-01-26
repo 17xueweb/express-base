0a299832db48b6c3b878be2d05b5f6677522feee
lcruan <1416802331@qq.com>
2024-01-26 22:20:53 +0800

    express.static 静态资源挂载路径前缀

--- 01-express基本使用/02.使用express.static对外提供静态资源.js
+++ 01-express基本使用/02.使用express.static对外提供静态资源.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const app = express()
 
-// 在这里，调用 express.static() 方法，快速的对外提供静态资源
-// 托管多个静态资源目录，如果两个里面都有index.html 谁写在前面 就展示谁
+// （1）在这里，调用 express.static() 方法，快速的对外提供静态资源
+// （2）托管多个静态资源目录，如果两个里面都有index.html 谁写在前面 就展示谁
 // 通过页面访问的时候，路径中不需要写clock http://127.0.0.1/index.html
-app.use(express.static('clock'))
+//  (3)  静态资源前面增加 前缀 app.use('/clock', xxx)
+app.use('/clock', express.static('clock'))
 app.use(express.static('files'))
 
 app.listen(80, () => {
